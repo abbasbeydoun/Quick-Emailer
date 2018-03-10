89f5154741a8e7b3b4dc7cc5262332c1d93aa0dc
abbasbaydoun <abbasbaydoun717@gmail.com>
2018-03-10 18:14:07 -0500

    - Prompt to enter gmail email and password - Email and password stores in text file in home path, password is encrypted with a key

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print 'success'
+import smtplib
+import os.path
+from Crypto.Cipher import ARC4
+
+
+if os.path.isfile('creds.txt'):
+
+    arc4_decrypt = ARC4.new('somekey')
+    readfile = open('creds.txt', 'r')
+    lineArray = readfile.readline().split(':')
+    email = lineArray[0]
+    encryptedPassword = lineArray[1]
+    password = arc4_decrypt.decrypt(encryptedPassword)
+
+    print 'Email is '+email+' and passsword is '+password
+
+else:
+
+    arc4_encrypt = ARC4.new('somekey')
+    email = raw_input('E-mail: ')
+    password = raw_input('Password: ')
+
+    encryptedPassword = arc4_encrypt.encrypt(password)
+
+    with open('creds.txt', 'w') as f:
+
+        print 'Email is '+email
+        print 'EncryptedPassword is '+encryptedPassword
+
+        f.write(email+":"+encryptedPassword)
+
+        f.close()
